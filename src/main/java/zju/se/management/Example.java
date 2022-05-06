8c4d7acc496f32156b731a25d5339ceb27bdaba2
Zhiyuan Pan <zy_pan@zju.edu.cn>
2022-05-06 13:33:45 +0800

    Add json example

---
+++ src/main/java/zju/se/management/Example.java
@@ -0,0 +1,4 @@
+package zju.se.management;
+
+public record Example(int id, String name) {
+}
--- src/main/java/zju/se/management/ExampleController.java
+++ src/main/java/zju/se/management/ExampleController.java
@@ -1,13 +1,19 @@
 package zju.se.management;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExampleController {
 
     @GetMapping("/test")
-    String test(){
+    public String test(){
         return "Hello world";
+    }
+
+    @GetMapping("/json")
+    public Example JSONExample(@RequestParam(value = "name", defaultValue = "user") String name){
+        return new Example(1, name);
     }
 }
