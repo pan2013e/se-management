6388e44a90f0f01cdfb536c44d6c39601e33118e
Zhiyuan Pan <zy_pan@zju.edu.cn>
2022-06-04 15:15:25 +0800

    reset ArrangeListResponseData

--- src/main/java/zju/se/management/utils/ArrangeListResponseData.java
+++ src/main/java/zju/se/management/utils/ArrangeListResponseData.java
@@ -5,17 +5,15 @@ import lombok.Setter;
 import zju.se.management.entity.Arrange;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Getter
 @Setter
 public class ArrangeListResponseData extends ResponseData {
-    private final List<ArrangeResponseData> arranges;
+    private final List<List<Date>> arranges;
 
-    public ArrangeListResponseData(List<Arrange> arranges){
-        this.arranges = new ArrayList<>(arranges.size());
-        for (Arrange arrange : arranges) {
-            this.arranges.add(new ArrangeResponseData(arrange));
-        }
+    public ArrangeListResponseData(List<List<Date>> arranges){
+        this.arranges = arranges;
     }
 }
