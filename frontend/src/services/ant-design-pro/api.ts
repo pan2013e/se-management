f140a292687fc97c77d026e0f972190c2b2bbc68
Zhiyuan Pan <zy_pan@zju.edu.cn>
2022-06-05 14:37:22 +0800

    modify api call

--- frontend/src/services/ant-design-pro/api.ts
+++ frontend/src/services/ant-design-pro/api.ts
@@ -13,7 +13,6 @@ export async function currentUser(
 ) {
     return request<API.BackendResult>(`${baseUrl}/oauth/verify`, {
         method: 'GET',
-        params: body,
         headers: {
             'token': localStorage.getItem('token') || '',
         },
