1aa8264fa27fc6cae2224ef54ca61e170221c586
Zhiyuan Pan <zy_pan@zju.edu.cn>
2022-06-10 11:44:20 +0800

    Set Tomcat I/O mode to APR

--- src/main/java/zju/se/management/ManagementApplication.java
+++ src/main/java/zju/se/management/ManagementApplication.java
@@ -2,11 +2,21 @@ package zju.se.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 @EnableRedisHttpSession
 public class ManagementApplication {
+
+    @Bean
+    public ConfigurableServletWebServerFactory webServerFactory(){
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.setProtocol("org.apache.coyote.http11.Http11AprProtocol");
+        return factory;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ManagementApplication.class, args);
